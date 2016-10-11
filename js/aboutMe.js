49ded01fe52e6fc5f58b86c4c49bad9ff9d2411b
vicky0203 <qweileen@gmail.com>
2016-10-11 23:26:50 +0800

    redesign resume, need add json data

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,40 +1,19 @@
 $(document).ready(function(){
-    $(".title").click(function(){
-        // console.log(this);
+    $(".chapter .title").click(function(){
         var activetitle = $(this);
+
         if(activetitle.hasClass("active")){
-            console.log("aaa");
              activetitle.nextAll().fadeOut("1000");
-             activetitle.children("span").text("+");
-             activetitle.children("i").css("background-position","0 -45px");
+             activetitle.children(".hide-info-icon").toggle();
+             activetitle.children(".show-info-icon").toggle();
+             activetitle.children(".title-icon").css("background-position","0 -45px");
              activetitle.removeClass("active");
         }else{
              activetitle.addClass("active");
              activetitle.nextAll().fadeIn("1000");
-             activetitle.children("span").text("-");
-             activetitle.children("i").css("background-position","0 0");
+             activetitle.children(".hide-info-icon").toggle();
+             activetitle.children(".show-info-icon").toggle();
+             activetitle.children(".title-icon").css("background-position","0 0");
         }
-    });
-
-    //menu
-    $(".menuicon").mouseover(function(){
-        // $(".menubar").fadeIn(1000);
-         $(".menubar").animate({"height":'toggle'},"1");
-    });
-    $(".closeicon").click(function(){
-        $(".menubar").animate({"height":'toggle'},"1");
-    });
-
-    $.ajax({
-        type: "GET",
-        url: "/resume_en.json",
-        dataType: "json",
-        success: function(data){
-            console.log(data);
-        },
-        error: function(data){
-            alert('error');
-        }
-
-    });
+    }); 
 });
