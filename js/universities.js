3b32644f3fe46fc489f6f9e37037a949460cd414
vicky0203 <qweileen@gmail.com>
2016-10-13 00:26:04 +0800

    fix gallery pic position

--- js/universities.js
+++ js/universities.js
@@ -5,15 +5,7 @@ var colHeight = new Array(colNum); //sum heights for each columns
 
 $(document).ready(function(){
 
- 	$(".box").each(function(i, box){
- 		var picHeight = $(box).outerHeight(true);
-
- 		if (i < colNum) {
- 			colHeight[i] = picHeight;
- 		}else{
- 			placePic(box, picHeight);
- 		}
- 	});
+ 	placePics();
 
  	$(window).scroll(function(){  
     // 判断窗口的滚动条是否接近页面底部，这里的20可以自定义  
@@ -23,35 +15,42 @@ $(document).ready(function(){
             	console.log(i);
             	var newbox = document.createElement("span");
             	newbox.setAttribute("class", "box");
+            	newbox.setAttribute("id", "box" + i);
             	gallery.append(newbox);
             	var newpic = document.createElement("img");
             	newpic.setAttribute("src", "images/gallery/" + i + ".jpg");
-            	newbox.append(newpic);
-
-            	var newpicHeigh = $(newbox).outerHeight(true);
-            	placePic(newbox, newpicHeigh);
+            	newbox.appendChild(newpic);
             }
         }
+
+        placePics();
     });
 }); 
 
-function placePic(box, picHeight){
-	var currentMinHeight = Math.min.apply(this, colHeight);
-	var currentMinHeightIndex = 0; 
-	for(var i = 0; i < colHeight.length; i++){ //IE 不支持 indexOf
-		if( colHeight[i] == currentMinHeight){
-			currentMinHeightIndex = i;
-			break;
-		}
-	}
+function placePics(){
+	$(".box").each(function(i, box){
+ 		var picHeight = $(box).outerHeight(true);
 
-	$(box).css({
-		"position" : "absolute",
-		"top"  : currentMinHeight,
-		"left" : $(".box").eq(currentMinHeightIndex).position().left
-	})
+ 		if (i < colNum) {
+ 			colHeight[i] = picHeight;
+ 		}else{
+ 			var currentMinHeight = Math.min.apply(this, colHeight);
+ 			var currentMinHeightIndex = 0; 
+ 			for(var i = 0; i < colHeight.length; i++){ //IE 不支持 indexOf
+ 				if( colHeight[i] == currentMinHeight){
+ 					currentMinHeightIndex = i;
+ 					break;
+ 				}
+ 			}
 
-	colHeight[currentMinHeightIndex] += picHeight;
-	gallery.css("min-height", Math.max.apply(this, colHeight));
- 		
+ 			$(box).css({
+ 				"position" : "absolute",
+ 				"top"  : currentMinHeight,
+ 				"left" : $(".box").eq(currentMinHeightIndex).position().left
+ 			})
+
+ 			colHeight[currentMinHeightIndex] += picHeight;
+ 			gallery.css("height", Math.max.apply(this, colHeight));
+ 		}
+ 	});	
 }
