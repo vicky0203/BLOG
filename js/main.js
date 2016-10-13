fa2de52a44333ff935c659c10ef69814f70b5b11
vicky0203 <qweileen@gmail.com>
2016-10-13 22:36:03 +0800

    map done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var current = 0;
 
 $(document).ready(function() {
 
+    //imgs carousel
     $(".carousel ul li").click(function(){
         var i = $(this).val();
 
@@ -26,6 +27,46 @@ $(document).ready(function() {
     },function(){
         timer = setInterval("autoShowImage()", 2000);
     });
+
+    //map
+    var map = new BMap.Map("journey-map");          // 创建地图实例  
+    var point = new BMap.Point(109.111,32.642);  // 创建点坐标  
+    map.centerAndZoom(point, 5);                 // 初始化地图，设置中心点坐标和地图级别  
+    var opts = {type: BMAP_NAVIGATION_CONTROL_ZOOM};  
+    map.addControl(new BMap.NavigationControl(opts));
+  
+    var home = new BMap.Point(117.198227,39.099922)
+    var markerHome = new BMap.Marker(home);  // 创建标注
+    map.addOverlay(markerHome);               // 将标注添加到地图中
+    markerHome.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+
+    var hk = new BMap.Point(114.144276,22.287457);
+    var markerHK = new BMap.Marker(hk);  // 创建标注
+    map.addOverlay(markerHK);               // 将标注添加到地图中
+
+    var tw = new BMap.Point(120.98807,23.514679);
+    var markerTW = new BMap.Marker(tw);  // 创建标注
+    map.addOverlay(markerTW);               // 将标注添加到地图中
+
+    var cd = new BMap.Point(104.032887,30.665732);
+    var markerCD = new BMap.Marker(cd);  // 创建标注
+    map.addOverlay(markerCD);               // 将标注添加到地图中
+
+    var jzg = new BMap.Point(103.918846,33.164503);
+    var markerJZG = new BMap.Marker(jzg);  // 创建标注
+    map.addOverlay(markerJZG);               // 将标注添加到地图中
+
+    var curve1 = new BMapLib.CurveLine([home, hk], {strokeColor:"blue", strokeWeight:3, strokeOpacity:0.5}); //创建弧线对象
+    map.addOverlay(curve1); //添加到地图中
+
+    var curve2 = new BMapLib.CurveLine([hk, tw], {strokeColor:"green", strokeWeight:3, strokeOpacity:0.5}); //创建弧线对象
+    map.addOverlay(curve2); //添加到地图中
+
+    var curve3 = new BMapLib.CurveLine([home, cd], {strokeColor:"green", strokeWeight:3, strokeOpacity:0.5}); //创建弧线对象
+    map.addOverlay(curve3); //添加到地图中
+
+    var curve4 = new BMapLib.CurveLine([cd, jzg], {strokeColor:"green", strokeWeight:3, strokeOpacity:0.5}); //创建弧线对象
+    map.addOverlay(curve4); //添加到地图中
 
 });
 
